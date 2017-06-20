3ba9a356a04e9e246b89a2fbc032f2d6d7fbfc60
naseehaps <naseehaps@gmail.com>
2017-06-20 23:01:40 +0530

    First commit

---
+++ a.py
@@ -0,0 +1,26 @@
+def print_one():
+    print  '+', '-', '-', '-', '-', '+', '-', '-', '-', '-', '+'
+def print_four():
+    print '/',
+    print ' ',
+    print ' ',
+    print ' ',
+    print ' ',
+    print '/',
+    print ' ',
+    print ' ',
+    print ' ',
+    print ' ',
+    print '/'
+print_one()
+print_four()
+print_four()
+print_four()
+print_four()
+print_one()
+print_four()
+print_four()
+print_four()
+print_four()
+print_one()
+
